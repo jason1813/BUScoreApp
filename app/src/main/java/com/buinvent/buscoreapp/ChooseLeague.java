043ca6a0434aa494f4d24094ad6a39617db79073
SlinkyMation <jmmorris2@mail.bradley.edu>
2017-12-23 14:20:40 -0600

    initial

---
+++ app/src/main/java/com/buinvent/buscoreapp/ChooseLeague.java
@@ -0,0 +1,13 @@
+package com.buinvent.buscoreapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ChooseLeague extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_league);
+    }
+}
