7b74232adb521c7a37508053a980b9cb89d910ae
SlinkyMation <jmmorris2@mail.bradley.edu>
2017-12-31 14:12:51 -0600

    added ChooseMatchup but haven't started on calls to API

---
+++ app/src/main/java/com/buinvent/buscoreapp/ChooseMatchupActivity.java
@@ -0,0 +1,13 @@
+package com.buinvent.buscoreapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ChooseMatchupActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.choose_matchup);
+    }
+}
