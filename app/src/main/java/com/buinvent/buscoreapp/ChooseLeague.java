82f87f143469c5a3a3daa13c7b73429fd7e6ae7e
SlinkyMation <jmmorris2@mail.bradley.edu>
2018-01-01 12:08:23 -0600

    go to the NFL page when NFL button is clicked

--- app/src/main/java/com/buinvent/buscoreapp/ChooseLeague.java
+++ app/src/main/java/com/buinvent/buscoreapp/ChooseLeague.java
@@ -1,7 +1,10 @@
 package com.buinvent.buscoreapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ChooseLeague extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class ChooseLeague extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_league);
+
+
+        Button btn = (Button)findViewById(R.id.nfl_btn);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(ChooseLeague.this, ChooseMatchupActivity.class));
+            }
+        });
+
     }
 }
