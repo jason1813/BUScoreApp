6561eeaeea4d4b3aaf1dcd265366bc3c22ef934a
SlinkyMation <jmmorris2@mail.bradley.edu>
2018-01-07 16:15:56 -0600

    got the content on the Avengers API link to print on on matchup execute

--- app/src/main/java/com/buinvent/buscoreapp/ChooseMatchupActivity.java
+++ app/src/main/java/com/buinvent/buscoreapp/ChooseMatchupActivity.java
@@ -1,7 +1,16 @@
 package com.buinvent.buscoreapp;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class ChooseMatchupActivity extends AppCompatActivity {
 
@@ -9,5 +18,62 @@ public class ChooseMatchupActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.choose_matchup);
+        new RetrieveFeedTask().execute();
+
     }
+
+    class RetrieveFeedTask extends AsyncTask<Void, Void, String> {
+
+        protected void onPreExecute() {
+        }
+
+        protected String doInBackground(Void... urls) {
+
+            try {
+                URL url = new URL("https://jsonparsingdemo-cec5b.firebaseapp.com/jsonData/moviesDemoItem.txt");
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                try {
+                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+                    StringBuilder stringBuilder = new StringBuilder();
+                    String line;
+                    while ((line = bufferedReader.readLine()) != null) {
+                        stringBuilder.append(line).append("\n");
+                    }
+                    bufferedReader.close();
+                    return stringBuilder.toString();
+                }
+                finally{
+                    urlConnection.disconnect();
+                }
+            }
+            catch(Exception e) {
+                Log.e("ERROR", e.getMessage(), e);
+                return null;
+            }
+        }
+
+        protected void onPostExecute(String response) {
+            if(response == null) {
+                response = "THERE WAS AN ERROR";
+            }
+            Log.i("INFO", response);
+            System.out.println(response);
+            // TODO: check this.exception
+            // TODO: do something with the feed
+
+//            try {
+//                JSONObject object = (JSONObject) new JSONTokener(response).nextValue();
+//                String requestID = object.getString("requestId");
+//                int likelihood = object.getInt("likelihood");
+//                JSONArray photos = object.getJSONArray("photos");
+//                .
+//                .
+//                .
+//                .
+//            } catch (JSONException e) {
+//                e.printStackTrace();
+//            }
+        }
+    }
+
 }
