292fba8cdab673b8dea25bcbee5d3a643aca7d5a
Jason Morris <jmmorris2@mail.bradley.edu>
2018-01-09 22:16:27 -0600

    Trying to dynamically add buttons for each matchup along with getting matchup data from api
    This commit only proves that we can dynamically add a button and can get JSON data for matchups.

--- app/src/main/java/com/buinvent/buscoreapp/ChooseMatchupActivity.java
+++ app/src/main/java/com/buinvent/buscoreapp/ChooseMatchupActivity.java
@@ -4,6 +4,13 @@ import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.LinearLayout.LayoutParams;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
@@ -24,8 +31,16 @@ public class ChooseMatchupActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_matchup);
+
+        Button myButton = new Button(this);
+        myButton.setText("Push Me");
+
+        LinearLayout ll = (LinearLayout)findViewById(R.id.matchup_layout);
+        LayoutParams lp = new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT);
+        ll.addView(myButton, lp);
 
         // Set API Authentication.
         Authenticator.setDefault(new Authenticator() {
@@ -70,6 +85,22 @@ public class ChooseMatchupActivity extends AppCompatActivity {
         protected void onPostExecute(String response) {
             Log.v(TAG, "HTTP Response: " + response);
             // TODO: do something with the feed
+
+            try{
+
+                JSONObject obj = new JSONObject(response);
+                JSONArray games = obj.getJSONObject("dailygameschedule").getJSONArray("gameentry");
+
+                String awayteam1 = games.getJSONObject(0).getJSONObject("awayTeam").getString("Name");
+                String hometeam1 = games.getJSONObject(0).getJSONObject("homeTeam").getString("Name");
+
+                System.out.println("awayteam1 = " + awayteam1);
+                System.out.println("hometeam1 = " + hometeam1);
+
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
 
 //            try {
 //                JSONObject object = (JSONObject) new JSONTokener(response).nextValue();
